e670bcbed14c62df7f734b5451d8a3e5d39bd4b3
Анастасия <bobkova-ao@yandex-team.ru>
2020-12-27 01:57:54 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Определение основных принципов анализа и типологизации финансовых моделей деятельности ВУЗов',
+    author='nasty',
+    license='MIT',
+)
